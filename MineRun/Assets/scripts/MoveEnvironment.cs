0cc8574ec2dd2d3bbbf15552c802a232789e80d4
SteveHutchison <stevehutchison360@hotmail.co.uk>
2016-03-06 23:58:02 +0000

    updated animations plus falling objects
    as the summary suggests. Also some fixes to old scripts involving
    environment moving. Added tons of new image files old placeholders still
    need removed from the project.

--- MineRun/Assets/scripts/MoveEnvironment.cs
+++ MineRun/Assets/scripts/MoveEnvironment.cs
@@ -6,11 +6,19 @@ public class MoveEnvironment : MonoBehaviour {
 	Transform cameraTrans;
 	GameObject cam;
 	public Vector3 pos;
+    private Rigidbody2D rb2d;
+    public bool working;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		cam = GameObject.FindGameObjectWithTag("MainCamera");
-	}
+        if (transform.tag == "Falling")
+        {
+            rb2d = gameObject.GetComponent<Rigidbody2D>();
+        }
+        working = false;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -22,13 +30,21 @@ public class MoveEnvironment : MonoBehaviour {
 		if (pos.x + 38 < cameraTrans.position.x) {
 			pos.x += 19*4;
 
-			/*
+            if (transform.tag == "Falling")
+            {
+                working = true;
+                pos.y = 8.2f;
+                rb2d.gravityScale = 0;
+                rb2d.velocity *= 0;
+            }
+
+            /*
 			if(transform.tag == "Wall")
 			{
 				pos.x += 26;
 				ScoreBoard.AddScore (10);
 			}*/
-			transform.position = pos;
+            transform.position = pos;
 
 		}
 
